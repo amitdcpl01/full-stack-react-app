d1a53d828e76c5cd194d48082a7390ceab307e48
AMIT KUMAR <amitkumar028@in.ibm.com>
2020-07-20 01:27:08 +0530

    adding comment

--- complete-react-developer/client/src/components/post/Post.js
+++ complete-react-developer/client/src/components/post/Post.js
@@ -6,6 +6,8 @@ import Spinner from '../layout/Spinner';
 import { getPost } from '../../action/post';
 import PostItem from '../posts/PostItem';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
 
@@ -17,6 +19,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         loading || post === null ? <Spinner /> : <Fragment>
             <Link to='/posts' className='btn'>Back to Posts</Link>
             <PostItem post={post} showActions={false}></PostItem>
+            <CommentForm postId={post._id}></CommentForm>
+            <div className="comments">
+                {post.comments.map(comment => (
+                    <CommentItem key={comment._id} comment={comment} postId={post._id}></CommentItem>
+                ))}
+            </div>
         </Fragment>
     )
 }
