7148e389b07bb2f7bfed7b150db76cd704d59279
AMIT KUMAR <amitkumar028@in.ibm.com>
2020-07-20 22:08:32 +0530

    add not found page

--- complete-react-developer/client/src/App.js
+++ complete-react-developer/client/src/App.js
@@ -2,28 +2,28 @@ import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Alert from "./components/layout/Alert";
+// import Register from "./components/auth/Register";
+// import Login from "./components/auth/Login";
+// import Alert from "./components/layout/Alert";
 import { loadUser } from "./action/auth";
 import setAuthToken from "./utils/setAuthToken";
-import Dashboard from './components/dashboard/Dashboard';
-
-
+// import Dashboard from './components/dashboard/Dashboard';
+// import NotFound from './components/layout/NotFound';
 import "./App.css";
 
 //redux
 import { Provider } from "react-redux";
 import store from "./store";
-import PrivateRoute from "./components/routing/PrivateRoute";
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import EditProfiles from "./components/profile-forms/EditProfiles";
-import AddExperience from "./components/profile-forms/AddExperience";
-import AddEducation from "./components/profile-forms/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
-import Posts from "./components/posts/Posts";
-import Post from "./components/post/Post";
+// import PrivateRoute from "./components/routing/PrivateRoute";
+// import CreateProfile from "./components/profile-forms/CreateProfile";
+// import EditProfiles from "./components/profile-forms/EditProfiles";
+// import AddExperience from "./components/profile-forms/AddExperience";
+// import AddEducation from "./components/profile-forms/AddEducation";
+// import Profiles from "./components/profiles/Profiles";
+// import Profile from "./components/profile/Profile";
+// import Posts from "./components/posts/Posts";
+// import Post from "./components/post/Post";
+import Routes from './components/routing/Routes';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -39,8 +39,11 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+          {/* <section className='container'>
             <Alert />
             <Switch>
               <Route exact path='/register' component={Register} />
@@ -54,8 +57,10 @@ const App = () => {
               <PrivateRoute exact path='/add-education' component={AddEducation} />
               <PrivateRoute exact path='/posts' component={Posts} />
               <PrivateRoute exact path='/posts/:id' component={Post} />
+              <Route component={NotFound}></Route>
+
             </Switch>
-          </section>
+          </section> */}
         </Fragment>
       </Router>
     </Provider>
